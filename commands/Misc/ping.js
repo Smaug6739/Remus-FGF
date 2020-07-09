eb820813e8691ccd990e158f9a7d1c4a3daac083
raphael veauville <veauville.raphael@gmail.com>
2020-07-09 20:46:41 +0200

    Update ststus and status add in config.js

--- commands/Misc/ping.js
+++ commands/Misc/ping.js
@@ -3,16 +3,7 @@ module.exports.run =async(client, message, args) => {
     let debut = Date.now();
     message.channel.send('Pong !').then(async(m) => await m.edit(`Pong  BOT : \`${Date.now()-debut}ms\` API : \`${client.ws.ping}ms\``));
 
-    let text = args.join(' ');
-    let result = '';
-    for (let i = 0; i < text.length; i++) {
-        let bin = text[i].charCodeAt().toString(2);
-        result += Array(8 - bin.length + 1).join('0') + bin;
-    }
-    return message.channel.send({ embed: {
-        title: 'Binary converssion',
-        description: result
-    }});
+
     
 
 
